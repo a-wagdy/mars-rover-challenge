19b8f9c6ddd839f6350adaba472da08b5f0fc3d3
Ahmed Wagdy <ahmed@pelcro.com>
2024-04-09 04:51:17 +0200

    Replaced switch with match case

--- App/Commands/MoveForwardCommand.php
+++ App/Commands/MoveForwardCommand.php
@@ -15,20 +15,12 @@ class MoveForwardCommand implements CommandInterface
         $newX = $rover->getPosition()->getX();
         $newY = $rover->getPosition()->getY();
 
-        switch ($rover->getDirection()) {
-            case Direction::North:
-                $newY++;
-                break;
-            case Direction::East:
-                $newX++;
-                break;
-            case Direction::South:
-                $newY--;
-                break;
-            case Direction::West:
-                $newX--;
-                break;
-        }
+        match ($rover->getDirection()) {
+            Direction::North => $newY++,
+            Direction::East => $newX++,
+            Direction::South => $newY--,
+            Direction::West => $newX--,
+        };
 
         if ($newX < 0 || $newX > $rover->getPlateau()->getX() || $newY < 0 || $newY > $rover->getPlateau()->getY()) {
             throw new InvalidArgumentException("Rover cannot move beyond plateau boundaries");
