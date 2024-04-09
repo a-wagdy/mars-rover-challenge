dac411ede84e0d533a581c3cb1a010d18773862a
Ahmed Wagdy <ahmed@pelcro.com>
2024-04-09 15:40:40 +0200

    Modified the input to accept multiple rovers

--- index.php
+++ index.php
@@ -10,39 +10,19 @@ use App\Models\Plateau;
 use App\Models\Position;
 use App\Models\Rover;
 
-function runTestCases(Plateau $plateau): void
+function run(Plateau $plateau, array $rovers): void
 {
-    $testCases = [
-        [
-            'rover' => [1, 2, Direction::North, 'M'],
-            'expected' => '1 3 N',
-        ],
-        [
-            'rover' => [1, 2, Direction::North, 'RRLL'],
-            'expected' => '1 2 N',
-        ],
-        [
-            'rover' => [1, 2, Direction::North, 'LMLMLMLMM'],
-            'expected' => '1 3 N',
-        ],
-        [
-            'rover' => [3, 3, Direction::East, 'MMRMMRMRRM'],
-            'expected' => '5 1 E',
-        ],
-    ];
+    foreach ($rovers as $roverData) {
+        $position = new Position($roverData[0], $roverData[1]);
+        $rover = new Rover($plateau, $position, $roverData[2]);
 
-    foreach ($testCases as $testCase) {
-        $position = new Position($testCase['rover'][0], $testCase['rover'][1]);
-        $direction = $testCase['rover'][2];
-        $rover = new Rover($plateau, $position, $direction);
-
-        $commands = $testCase['rover'][3];
-        $commands = str_split($commands);
+        $commands = str_split($roverData[3]);
 
         CommandService::executeCommands($rover, $commands);
 
         $actualPosition = $rover->getCurrentPosition();
-        $expectedPosition = $testCase['expected'];
+
+        $expectedPosition = $roverData[4];
 
         if ($actualPosition === $expectedPosition) {
             echo "Test Passed - Expected: $expectedPosition, Actual: $actualPosition\n";
@@ -52,5 +32,12 @@ function runTestCases(Plateau $plateau): void
     }
 }
 
-$plateau = new Plateau(5, 5);
-runTestCases($plateau);
+$plateau = new Plateau(5,5);
+$rovers = [
+    [1, 2, Direction::North, 'M', '1 3 N'],
+    [1, 2, Direction::North, 'RRLL', '1 2 N'],
+    [1, 2, Direction::North, 'LMLMLMLMM', '1 3 N'],
+    [3, 3, Direction::East, 'MMRMMRMRRM', '5 1 E'],
+];
+
+run($plateau, $rovers);
