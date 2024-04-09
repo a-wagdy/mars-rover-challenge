4ee225056a35624b8686e30febc4975a8b82b1cf
Ahmed Wagdy <ahmed@pelcro.com>
2024-04-09 16:49:09 +0200

    Created PHPUnit test cases

---
+++ tests/RoverTest.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Models\Plateau;
+use App\Models\Position;
+use App\Models\Rover;
+use PHPUnit\Framework\TestCase;
+use App\Enums\Direction;
+use App\CommandService;
+
+final class RoverTest extends TestCase
+{
+    public function testCommandExecution(): void
+    {
+        $plateau = new Plateau(5, 5);
+
+        // Index number 4 is the expected output
+        $rovers = [
+            [1, 2, Direction::North, 'M', '1 3 N'],
+            [1, 2, Direction::North, 'RRLL', '1 2 N'],
+            [1, 2, Direction::North, 'LMLMLMLMM', '1 3 N'],
+            [3, 3, Direction::East, 'MMRMMRMRRM', '5 1 E'],
+        ];
+
+        foreach ($rovers as $roverData) {
+            $position = new Position($roverData[0], $roverData[1]);
+            $rover = new Rover($plateau, $position, $roverData[2]);
+
+            $commands = str_split($roverData[3]);
+
+            $commandService = new CommandService($rover, $commands);
+            $commandService->executeCommands();
+
+            $actualOutput = $rover->getCurrentPosition();
+            $expectedOutput = $roverData[4];
+
+            $this->assertEquals($actualOutput, $expectedOutput);
+
+        }
+    }
+}
