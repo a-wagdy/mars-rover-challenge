abe3510349d0b9fe2b54fe662f2e8b7675e6d813
Ahmed Wagdy <ahmed@pelcro.com>
2024-04-09 17:05:36 +0200

    Added more test cases

--- tests/RoverTest.php
+++ tests/RoverTest.php
@@ -21,6 +21,8 @@ final class RoverTest extends TestCase
             [1, 2, Direction::North, 'RRLL', '1 2 N'],
             [1, 2, Direction::North, 'LMLMLMLMM', '1 3 N'],
             [3, 3, Direction::East, 'MMRMMRMRRM', '5 1 E'],
+            [0, 0, Direction::South, 'RRRR', '0 0 S'],
+            [0, 0, Direction::West, '', '0 0 W'],
         ];
 
         foreach ($rovers as $roverData) {
@@ -37,6 +39,27 @@ final class RoverTest extends TestCase
 
             $this->assertEquals($actualOutput, $expectedOutput);
 
+        }
+    }
+
+    public function testMoveBeyondPlateauDimension(): void
+    {
+        $plateau = new Plateau(5, 5);
+
+        $rovers = [
+            [4, 4, Direction::North, 'MMMMMM'],
+        ];
+
+        foreach ($rovers as $roverData) {
+            $position = new Position($roverData[0], $roverData[1]);
+            $rover = new Rover($plateau, $position, $roverData[2]);
+
+            $commands = str_split($roverData[3]);
+
+            $this->expectException(InvalidArgumentException::class);
+
+            $commandService = new CommandService($rover, $commands);
+            $commandService->executeCommands();
         }
     }
 }
