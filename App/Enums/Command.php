75d89f799867c7d63487640071214349177a9433
Ahmed Wagdy <ahmed@pelcro.com>
2024-04-09 04:46:59 +0200

    Used enum values

--- App/Enums/Command.php
+++ App/Enums/Command.php
@@ -18,9 +18,9 @@ enum Command: string
     public static function mapCommand(string $command): CommandInterface
     {
         return match ($command) {
-            'L' => new TurnLeftCommand(),
-            'R' => new TurnRightCommand(),
-            'M' => new MoveForwardCommand(),
+            Command::Left->value => new TurnLeftCommand(),
+            Command::Right->value => new TurnRightCommand(),
+            Command::Forward->value => new MoveForwardCommand(),
             default => throw new InvalidArgumentException("Invalid command: $command"),
         };
     }
